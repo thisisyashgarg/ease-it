d9b38d6bb6b3f0f069c1160bc66294cc721dcc55
Yash Garg <thisisyashgarg@gmail.com>
2022-12-27 19:55:44 +0530

    ask me anything added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # EaseIt
+
 ## EaseIt can write personalised messages for you, make your diet chart, debug your code of any programming language, explain any code and much more.
+
 [Visit EaseIt](https://easeit.cyclic.app)
--- app.js
+++ app.js
@@ -27,106 +27,134 @@ app.get("/", (req, res) => {
   res.render("main", { layout: "homepage.hbs" });
 });
 
-app.get("/messages", (req, res) => {
-  res.render("main", {
-    layout: "messages.hbs",
-    data: ``,
-    label: "Your Personalised Message",
+app
+  .route("/messages")
+  .get((req, res) => {
+    res.render("main", {
+      layout: "messages.hbs",
+      data: ``,
+      label: "Your Personalised Message",
+    });
+  })
+  .post((req, res) => {
+    const prompt = `Person A: ${req.body.replyToMessage}, Person B : ${req.body.replyToBeSent}, a better answer person B will give in response to Person A, in ${req.body.language} language, with ${req.body.sentiment} sentiment, with tone ${req.body.tone}`;
+    openAICall(prompt)
+      .then((ans) => {
+        console.log(ans);
+        res.render("main", {
+          layout: "messages.hbs",
+          data: `${ans.trimStart()}`,
+          label: "Your Personalised Message",
+          language: req.body.language,
+          replyToMessage: req.body.replyToMessage,
+          replyToBeSent: req.body.replyToBeSent,
+          sentiment: req.body.sentiment,
+          tone: req.body.tone,
+        });
+      })
+      .catch((err) => console.log(err));
   });
-});
 
-app.post("/messages", (req, res) => {
-  const prompt = `Person A: ${req.body.replyToMessage}, Person B : ${req.body.replyToBeSent}, a better answer person B will give in response to Person A, in ${req.body.language} language, with ${req.body.sentiment} sentiment, with tone ${req.body.tone}`;
-  openAICall(prompt)
-    .then((ans) => {
-      console.log(ans);
-      res.render("main", {
-        layout: "messages.hbs",
-        data: `${ans.trimStart()}`,
-        label: "Your Personalised Message",
-        language: req.body.language,
-        replyToMessage: req.body.replyToMessage,
-        replyToBeSent: req.body.replyToBeSent,
-        sentiment: req.body.sentiment,
-        tone: req.body.tone,
-      });
-    })
-    .catch((err) => console.log(err));
-});
-
-app.get("/dietchart", (req, res) => {
-  res.render("main", {
-    layout: "dietChart.hbs",
-    data: ``,
-    label: "Your Diet Chart",
+app
+  .route("/dietchart")
+  .get((req, res) => {
+    res.render("main", {
+      layout: "dietChart.hbs",
+      data: ``,
+      label: "Your Diet Chart",
+    });
+  })
+  .post((req, res) => {
+    const prompt = `Give me a full day diet plan of ${req.body.calories} calories including ${req.body.carbs} gm carbohydrates, ${req.body.fats} gm fats and ${req.body.protein} gm protein`;
+    openAICall(prompt)
+      .then((ans) => {
+        console.log(ans);
+        res.render("main", {
+          layout: "dietChart.hbs",
+          data: `${ans.trimStart()}`,
+          label: "Your Diet Chart",
+          calories: req.body.calories,
+          carbs: req.body.carbs,
+          protein: req.body.protein,
+          fats: req.body.fats,
+        });
+      })
+      .catch((err) => console.log(err));
   });
-});
 
-app.post("/dietchart", (req, res) => {
-  const prompt = `Give me a full day diet plan of ${req.body.calories} calories including ${req.body.carbs} gm carbohydrates, ${req.body.fats} gm fats and ${req.body.protein} gm protein`;
-  openAICall(prompt)
-    .then((ans) => {
-      console.log(ans);
-      res.render("main", {
-        layout: "dietChart.hbs",
-        data: `${ans.trimStart()}`,
-        label: "Your Diet Chart",
-        calories: req.body.calories,
-        carbs: req.body.carbs,
-        protein: req.body.protein,
-        fats: req.body.fats,
-      });
-    })
-    .catch((err) => console.log(err));
-});
-
-app.get("/debugcode", (req, res) => {
-  res.render("main", {
-    layout: "debugCode.hbs",
-    data: ``,
-    label: "Debugged Code",
-  });
-});
-
-app.post("/debugcode", (req, res) => {
-  const prompt = `, find errors in the code given below,
+app
+  .route("/debugcode")
+  .get((req, res) => {
+    res.render("main", {
+      layout: "debugCode.hbs",
+      data: ``,
+      label: "Debugged Code",
+    });
+  })
+  .post((req, res) => {
+    const prompt = `, find errors in the code given below,
     ${req.body.inputCode}`;
-  openAICall(prompt)
-    .then((ans) => {
-      console.log(ans);
-      res.render("main", {
-        layout: "debugCode.hbs",
-        data: `${ans.trimStart()}`,
-        label: "Debugged Code",
-        inputCode: `${req.body.inputCode}`,
-      });
-    })
-    .catch((err) => console.log(err));
-});
-
-app.get("/codeexplanation", (req, res) => {
-  res.render("main", {
-    layout: "codeExplanation.hbs",
-    data: ``,
-    label: "Explanation",
+    openAICall(prompt)
+      .then((ans) => {
+        console.log(ans);
+        res.render("main", {
+          layout: "debugCode.hbs",
+          data: `${ans.trimStart()}`,
+          label: "Debugged Code",
+          inputCode: `${req.body.inputCode}`,
+        });
+      })
+      .catch((err) => console.log(err));
   });
-});
 
-app.post("/codeexplanation", (req, res) => {
-  const prompt = `Explain the code given below,
+app
+  .route("/codeexplanation")
+  .get((req, res) => {
+    res.render("main", {
+      layout: "codeExplanation.hbs",
+      data: ``,
+      label: "Explanation",
+    });
+  })
+  .post((req, res) => {
+    const prompt = `Explain the code given below,
     ${req.body.inputCode}`;
-  openAICall(prompt)
-    .then((ans) => {
-      console.log(ans);
-      res.render("main", {
-        layout: "codeExplanation.hbs",
-        data: `${ans}`,
-        label: "Explanation",
-        inputCode: `${req.body.inputCode}`,
-      });
-    })
-    .catch((err) => console.log(err));
-});
+    openAICall(prompt)
+      .then((ans) => {
+        console.log(ans);
+        res.render("main", {
+          layout: "codeExplanation.hbs",
+          data: `${ans}`,
+          label: "Explanation",
+          inputCode: `${req.body.inputCode}`,
+        });
+      })
+      .catch((err) => console.log(err));
+  });
+
+app
+  .route("/ama")
+  .get((req, res) => {
+    res.render("main", {
+      layout: "askmeanything.hbs",
+      data: ``,
+      label: "Your response",
+    });
+  })
+  .post((req, res) => {
+    const prompt = `${req.body.ama}`;
+    openAICall(prompt)
+      .then((ans) => {
+        console.log(ans);
+        res.render("main", {
+          layout: "askmeanything.hbs",
+          data: `${ans}`,
+          label: "Your response",
+          ama: `${req.body.ama}`,
+        });
+      })
+      .catch((err) => console.log(err));
+  });
 
 app.listen(port, (req, res) => {
   console.log(`listening on ${port}`);
